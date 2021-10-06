2073384e9620e48d9731f4027fd360fccce52254
niravassar <assarn@objectcomputing.com>
2021-10-06 16:46:41 -0500

    add file

--- actions-build-guide.sh
+++ actions-build-guide.sh
@@ -5,5 +5,7 @@ export EXIT_STATUS=0
 
 curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
 chmod 777 build-guide
+
+echo "************** PRINTING **************"
 cat build-guide
 
