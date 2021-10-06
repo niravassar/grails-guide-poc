5643ee5d4baf8a5cef0acc50fdbc057762ce375a
niravassar <assarn@objectcomputing.com>
2021-10-06 16:49:10 -0500

    debig

--- actions-build-guide.sh
+++ actions-build-guide.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-export EXIT_STATUS=0
-
-curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
-chmod 777 build-guide
-
-echo "************** PRINTING **************"
-cat build-guide
+pwd
+ls -al
+#export EXIT_STATUS=0
+#
+#curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
+#chmod 777 build-guide
+#
+#echo "************** PRINTING **************"
+#cat build-guide
 
