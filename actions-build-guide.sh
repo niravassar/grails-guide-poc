9f5a241768f65e86c756238ebd24acc71c747130
niravassar <assarn@objectcomputing.com>
2021-10-07 10:30:23 -0500

    show you can run build-gradle.

--- actions-build-guide.sh
+++ actions-build-guide.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-pwd
-ls -al
-#export EXIT_STATUS=0
-#
-#curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
-#chmod 777 build-guide
-#
-#echo "************** PRINTING **************"
-#cat build-guide
+export EXIT_STATUS=0
+
+curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
+chmod 777 build-guide
+
+echo "************** PRINTING **************"
+cat build-guide
 
