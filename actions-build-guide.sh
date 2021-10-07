fc04aca52c5d923a626dc3f4437315f1fd9e9251
niravassar <assarn@objectcomputing.com>
2021-10-07 11:01:30 -0500

    invoke local my-build-guide

--- actions-build-guide.sh
+++ actions-build-guide.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
+## WORKS
+#export EXIT_STATUS=0
 
-echo "*****************************************"
-export EXIT_STATUS=0
+#curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
+#chmod 777 build-guide
 
-curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
-chmod 777 build-guide
-
-echo "************** PRINTING **************"
-cat build-guide
-echo "************** pwd *******************"
-pwd
-ls -al
+./my-build-guide
