80f993a2345344fdf036f62cd80251b7e1a46b9b
niravassar <assarn@objectcomputing.com>
2021-10-11 15:07:56 -0500

    push the folder

--- updateGuidesJson-and-push-gh-pages-script.sh
+++ updateGuidesJson-and-push-gh-pages-script.sh
@@ -9,32 +9,25 @@ export EXIT_STATUS=0
 export GUIDE_NAME=${PWD##*/}
 export GUIDE_BRANCH_TRIMMED=${GUIDE_BRANCH##*refs/heads/}
 
-##### WORKS
-#echo "***** Run updateGuidesJson to copy a new index into gh-pages"
-#
-#./gradlew updateGuidesJson || EXIT_STATUS=$?
-#if [[ $EXIT_STATUS -ne 0 ]]; then
-#    echo "updateGuidesJson failed"
-#    rm -rf gh-pages
-#    exit $EXIT_STATUS
-#fi
-#
-#echo "***** Copy guide docs into gh pages"
+echo "***** Run updateGuidesJson to copy a new index into gh-pages"
 
-#cd gh-pages
-#mkdir -p "$GUIDE_NAME"
-#cp -r ../build/docs/. "./$GUIDE_NAME/"
-#######################
+./gradlew updateGuidesJson || EXIT_STATUS=$?
+if [[ $EXIT_STATUS -ne 0 ]]; then
+    echo "updateGuidesJson failed"
+    rm -rf gh-pages
+    exit $EXIT_STATUS
+fi
+
+echo "***** Copy guide docs into gh pages"
 
 cd gh-pages
-echo "insert text here" >> myautofile.txt
-cat myautofile.txt
-ls -al
-echo "******** committing the file *********"
-git config --global user.name $GIT_NAME
-git config --global user.email $GIT_EMAIL
+mkdir -p "$GUIDE_NAME"
+cp -r ../build/docs/. "./$GUIDE_NAME/"
+
+echo "***** Pushing Grails Guide: $GUIDE_NAME; GITHUB_WORKFLOW: $GITHUB_WORKFLOW"
+
 git add .
-git commit -a -m "adding the myautofile.txt"
-git push
+git commit -a -m "***** Pushing Grails Guide: $GUIDE_NAME; GITHUB_WORKFLOW: $GITHUB_WORKFLOW"
+git push origin HEAD
 
 exit 0
