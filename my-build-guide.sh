2cd4b4db8e74abff06e68824ff14f93d164a1745
niravassar <assarn@objectcomputing.com>
2021-10-11 09:55:55 -0500

    add name

--- my-build-guide.sh
+++ my-build-guide.sh
@@ -3,7 +3,7 @@ set -e
 
 export EXIT_STATUS=0
 export GUIDE_NAME=${PWD##*/}
-export GUIDE_BRANCH_SHORT=${GUIDE_BRANCH##*refs/heads/}
+export GUIDE_BRANCH_TRIMMED=${GUIDE_BRANCH##*refs/heads/}
 
 ##### WORKS
 #echo "Publishing guide for branch $GITHUB_REPOSITORY"
