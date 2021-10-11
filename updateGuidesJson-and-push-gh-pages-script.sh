0108ce3bfdb2752b49a130b9041d336ad5ab0dcf
niravassar <assarn@objectcomputing.com>
2021-10-11 14:59:31 -0500

    try commit one file in .sh

--- updateGuidesJson-and-push-gh-pages-script.sh
+++ updateGuidesJson-and-push-gh-pages-script.sh
@@ -9,19 +9,32 @@ export EXIT_STATUS=0
 export GUIDE_NAME=${PWD##*/}
 export GUIDE_BRANCH_TRIMMED=${GUIDE_BRANCH##*refs/heads/}
 
-echo "***** Run updateGuidesJson to copy a new index into gh-pages"
+##### WORKS
+#echo "***** Run updateGuidesJson to copy a new index into gh-pages"
+#
+#./gradlew updateGuidesJson || EXIT_STATUS=$?
+#if [[ $EXIT_STATUS -ne 0 ]]; then
+#    echo "updateGuidesJson failed"
+#    rm -rf gh-pages
+#    exit $EXIT_STATUS
+#fi
+#
+#echo "***** Copy guide docs into gh pages"
 
-./gradlew updateGuidesJson || EXIT_STATUS=$?
-if [[ $EXIT_STATUS -ne 0 ]]; then
-    echo "updateGuidesJson failed"
-    rm -rf gh-pages
-    exit $EXIT_STATUS
-fi
-
-echo "***** Copy guide docs into gh pages"
+#cd gh-pages
+#mkdir -p "$GUIDE_NAME"
+#cp -r ../build/docs/. "./$GUIDE_NAME/"
+#######################
 
 cd gh-pages
-mkdir -p "$GUIDE_NAME"
-cp -r ../build/docs/. "./$GUIDE_NAME/"
+echo "insert text here" >> myautofile.txt
+cat myautofile.txt
+ls -al
+echo "******** committing the file *********"
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
+git add .
+git commit -a -m "adding the myautofile.txt"
+git push
 
 exit 0
